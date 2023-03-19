3f7de5bff4b611d92cf177a73f7ef9b2204e48da
samarpan sarkar <samarpansarkar209@gmail.com>
2023-03-19 16:56:09 +0530

    commit

---
+++ app/src/main/java/com/example/myapplicationfirebase/DoctorSearchActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplicationfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoctorSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_search);
+    }
+}
