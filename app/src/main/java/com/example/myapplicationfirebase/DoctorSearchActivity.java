60b3a1fe787cc4a19456fa5cf5e7b60558080289
samarpan sarkar <samarpansarkar209@gmail.com>
2023-04-04 20:36:41 +0530

    commit
    commit

--- app/src/main/java/com/example/myapplicationfirebase/DoctorSearchActivity.java
+++ app/src/main/java/com/example/myapplicationfirebase/DoctorSearchActivity.java
@@ -7,8 +7,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-import com.example.myapplicationfirebase.drSearch.Users;
-import com.example.myapplicationfirebase.drSearch.userAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,44 +16,11 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class DoctorSearchActivity extends AppCompatActivity {
-private RecyclerView DrrecyclerView;
-private userAdapter adapter;
-
-private ArrayList<Users> usersArrayList;
-
-private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_search);
 
-        database = FirebaseDatabase.getInstance();
-
-        DatabaseReference reference = database.getReference().child("user");
-
-        DrrecyclerView = findViewById(R.id.DrrecyclerView);
-        DrrecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        DrrecyclerView.setAdapter(adapter);
-
-        usersArrayList = new ArrayList<>();
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
-
-                    Users users = dataSnapshot.getValue(Users.class);
-                    usersArrayList.add(users);
-                }
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-        adapter = new userAdapter(DoctorSearchActivity.this,usersArrayList);
     }
-}
+};
